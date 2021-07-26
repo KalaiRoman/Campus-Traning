b4f2cd48202411d5439abf2943f9b427c1eaa672
KalaiRoman <86603627+KalaiRoman@users.noreply.github.com>
2021-07-26 10:41:25 +0530

    Reobizwebsite responsive

--- 19.7.2021/reobiz website/js/reobiz website.js
+++ 19.7.2021/reobiz website/js/reobiz website.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
-    dots: false,
+    autoplay: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
